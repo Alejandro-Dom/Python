8d790595ebad7cf2bceb9ac80d9678aea965c2a5
Alejandro-Dom <alejandrodomra@gmail.com>
2022-10-01 15:20:58 -0500

    Se agregan dos ejemplos más de estructuras de control

--- Estructuras de control/Iteraciones_for.py
+++ Estructuras de control/Iteraciones_for.py
@@ -1,5 +1,5 @@
 #Ingresa el límite de datos recopilados
-limit = int(input("Ingresa un número mayor a 3"))
+limit = int(input("Ingresa un número mayor a 3: "))
 #Muestra los números positivos en múltiplos de 3
 print("Los múltiplos de tres ", limit, " son: ")
 for i in range (3, limit + 1, 3):
